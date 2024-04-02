b8a2ee4c58ca73a70cf1ac887e6b9c95cd5f217a
Vladislav <vlad.freezzy@gmail.com>
2024-04-02 20:46:55 +0300

    added simple api-gateway

--- api-gateway/src/main/java/by/bsuir/apigateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/by/bsuir/apigateway/config/GatewayConfig.java
@@ -1,2 +1,18 @@
-package by.bsuir.apigateway.config;public class GatewayConfig {
+package by.bsuir.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-service", r -> r.path("/api/users/**")
+                        .uri("lb://user-service"))
+                .build();
+    }
 }
